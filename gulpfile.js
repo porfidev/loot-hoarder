d54be71403c0047fd0cf7fc9a2e0da3ab61eb780
elporfirio <elporfirio@gmail.com>
2016-11-14 07:41:11 -0600

    cambiesitos

--- gulpfile.js
+++ gulpfile.js
@@ -7,12 +7,37 @@ var gulp = require('gulp');
 
 var libs = [
     'node_modules/angular/angular.js',
-    'node_modules/angular-ui-router/release/angular-ui-router.js'
+    'node_modules/angular-ui-router/release/angular-ui-router.js',
+    'node_modules/bootstrap/dist/js/bootstrap.js'
 ];
 
-gulp.task('compilelibs', function(){
+var fonts = [
+    'node_modules/bootstrap/dist/fonts/*.*'
+];
+
+var css = [
+    'node_modules/bootstrap/dist/css/bootstrap.css',
+    'node_modules/bootstrap/dist/css/bootstrap-theme.css'
+];
+
+gulp.task('compile-libs', function(){
     return gulp.src(libs)
         //.pipe(debug({verbose: true}))
         //.pipe(uglify())
         .pipe(gulp.dest('public/lib'));
 });
+
+gulp.task('compile-fonts', function(){
+    return gulp.src(fonts)
+    //.pipe(debug({verbose: true}))
+    //.pipe(uglify())
+        .pipe(gulp.dest('public/fonts'));
+});
+
+gulp.task('compile-styles', function(){
+    return gulp.src(css)
+    //.pipe(debug({verbose: true}))
+    //.pipe(uglify())
+        .pipe(gulp.dest('public/css'));
+});
+
